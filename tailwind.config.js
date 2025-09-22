4f00783702a08ce298b6e6b039442b4421272efb
vasea <vasilii.costiuc@proton.me>
2025-09-22 17:12:36 +0300

    feat: initialize React project with Vite, Tailwind CSS, and ESLint
    - Added package.json with dependencies for React, React DOM, and React Router.
    - Configured PostCSS with Tailwind CSS and Autoprefixer.
    - Created a sample Vite logo SVG file.
    - Set up ESLint configuration for React and accessibility.
    - Implemented main App component with basic structure.
    - Added Tailwind CSS styles in main.css for components and utilities.
    - Configured entry point in main.jsx to render the App component.
    - Set up Tailwind CSS configuration for custom colors.
    - Created Vite configuration file for project setup.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import colors from 'tailwindcss/colors'
+ 
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.jsx'],
+  theme: {
+    extend: { colors },
+  },
+  plugins: [],
+}
